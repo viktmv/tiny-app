2aed00739edc87adf37fdab55b8eb54db063a94e
viktmv <viktmv@icloud.com>
2017-05-09 22:01:00 +0000

    add some more styles

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ let PORT  = process.env.PORT || 8080
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'))
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
+
 
 // ---> INDEX
 app.get('/', (req, res) => {
