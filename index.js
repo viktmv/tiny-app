3934a3c5ed55f48eaebee33c4884f138a9c175f2
viktmv <viktmv@icloud.com>
2017-05-10 20:33:48 +0000

    add login page and functionality

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ let urlDB = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 }
+let usersDB = {
+    "userRandomID": {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur"
+    },
+    "user2RandomID": {
+        id: "user2RandomID",
+        email: "user2@example.com",
+        password: "dishwasher-funk"
+      }
+  }
+
 // Initial app settings
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
@@ -21,57 +34,123 @@ app.get('/', (req, res) => {
 
 // --> /urls
 app.get('/urls', (req,res) => {
-  res.render('urls_index', { urls: urlDB, username: req.cookies["username"] })
+  let user = req.cookies['user_id'] ? usersDB[req.cookies["user_id"].user_id] : ''
+  let templateVars = {
+    urls: urlDB,
+    user: user
+  }
+  res.render('urls_index', templateVars)
+})
+
+// --> register page
+app.get('/register', (req,res) => {
+  let user = req.cookies['user_id'] ? usersDB[req.cookies["user_id"].user_id] : ''
+  let templateVars = {
+    urls: urlDB,
+    user
+  }
+  res.render('urls_register', templateVars)
+})
+
+// --> handle register req
+app.post('/register', (req,res) => {
+  let inDB = false
+  for (let key of Object.keys(usersDB)) {
+    if(usersDB[key].email == req.body.email)
+      inDB = true
+  }
+  if (inDB) return res.status(400).end('Reponded with 400')
+
+  let user_id = generateRandomString()
+  usersDB[user_id] = {
+    id: user_id,
+    email: req.body.email,
+    password: req.body.password
+    }
+  res.cookie('user_id', { user_id })
+  res.status(301).redirect('/urls')
 })
 
 // --> login
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username,
-    { expires: new Date(Date.now() + 900000 )}
-  )
-  res.redirect(`http://localhost:8080/urls`)
+  let userID = ''
+  for (let key of Object.keys(usersDB)) {
+    if(usersDB[key].email == req.body.email)
+      userID = usersDB[key].id
+  }
+  if (!userID) return res.status(403).end('No user found')
+
+  if (!(usersDB[userID].password == req.body.password)) return res.status(403).end('password does not match')
+
+  let templateVars = {
+    urls: urlDB,
+    user: usersDB[userID].id
+  }
+  res.cookie('user_id', { user_id: usersDB[userID].id })
+  res.status(301).redirect('/urls')
 })
 
+// --> login page
+app.get('/login', (req, res) => {
+  let user = req.cookies['user_id'] ? usersDB[req.cookies["user_id"].user_id] : ''
+  let templateVars = {
+    urls: urlDB,
+    user
+  }
+  res.render(`urls_login`, templateVars)
+})
+
+// --> logout
 app.post('/logout', (req, res) => {
-  res.clearCookie('username')
-  res.redirect(`http://localhost:8080/urls`)
+  res.clearCookie('user_id')
+  res.status(301).redirect(`http://localhost:8080/urls`)
 })
 
 // --> Add new address in DB
 app.post('/urls', (req, res) => {
   let shortURL = generateRandomString()
   urlDB[shortURL] = req.body.longURL
-  res.redirect(`http://localhost:8080/urls/${shortURL}`)
+  res.status(301).redirect(`http://localhost:8080/urls/${shortURL}`)
 })
 
 // --> Delete address from DB
 app.post('/urls/:id/delete', (req, res) => {
   console.log(req.params.id + 'deleted')
   delete urlDB[req.params.id]
-  res.redirect('/urls')
+  res.status(301).redirect('/urls')
 })
 
 // --> Update address in DB
 app.post('/urls/:id/update', (req, res) => {
   console.log(req.params.id + ' updated')
   urlDB[req.params.id] = req.body.longURL
-  res.redirect('/urls')
+  res.status(301).redirect('/urls')
 })
 
 // --> Render page to add new address
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new', { username: req.cookies["username"] })
+  let user = req.cookies['user_id'] ? usersDB[req.cookies["user_id"].user_id] : ''
+  let templateVars = {
+    urls: urlDB,
+    user
+  }
+  res.render('urls_new', templateVars)
 })
 
 // --> Render the update page
 app.get('/urls/:id', (req, res) => {
-  res.render('urls_show', { shortURL: req.params.id, username: req.cookies["username"] })
+  let user = req.cookies['user_id'] ? usersDB[req.cookies["user_id"].user_id] : ''
+  let templateVars = {
+    user,
+    shortURL: req.params.id
+  }
+  res.render('urls_show', templateVars)
 })
 
 // --> Redirection to long URLs
 app.get('/u/:shortURL', (req, res) => {
   let longURL = urlDB[req.params.shortURL]
-  res.redirect(longURL)
+  res.status(301).redirect(longURL)
 })
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
